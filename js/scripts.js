c5e806ab4a80f59bd01850b0837d7cd3e377ae62
darrion gering and jaime takikawa <jaime@>
2019-03-28 09:50:26 -0700

    add if else statement

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,18 @@
 //var age = parseInt(prompt("What is your age?"));
 
 $(document).ready(function() {
+  function ageCheck() {
     var age = prompt("How old are you?")
-    if(age => 18) {
-    $("#oldEnough").show();
+    if(age <= 18) {
+      $("#notOldEnough").show();
+    }
+    else if(age <= 100) {
+      $("#oldEnough").show();
+    }
+    else {
+      console.log("else");
+      // ageCheck();
+    }
   }
-  else {
-    $("#notOldEnough").show();
-  }
-  });
+  ageCheck();
+});
